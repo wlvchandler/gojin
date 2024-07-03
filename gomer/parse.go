d289f060ab68f889e01bc489c44cdcbb84a8f7cc
will chandler <jaskamiin@gmail.com>
2024-07-03 05:06:06 -0400

    regex based naive parser. DECLFUNC doesn't work yet - need to fix Perl rgx

--- gomer/parse.go
+++ gomer/parse.go
@@ -1,17 +1,29 @@
 package gomer
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"regexp"
+	"strings"
 )
 
 type Variable struct {
 	Type       string
 	Name       string
 	Visibility string
-	isStatic   bool
-	isConst    bool
+	IsStatic   bool
+	IsConst    bool
+}
+
+func NewVariable(_type string, _name string, _vis string, _static bool, _const bool) *Variable {
+	return &Variable{
+		Type:       _type,
+		Name:       _name,
+		Visibility: _vis,
+		IsStatic:   _static,
+		IsConst:    _const,
+	}
 }
 
 type Function struct {
@@ -19,8 +31,8 @@ type Function struct {
 	Name       string
 	Args       []Variable
 	Visibility string
-	isStatic   bool
-	isConst    bool
+	IsStatic   bool
+	IsConst    bool
 }
 
 type Enum struct {
@@ -37,6 +49,7 @@ type Class struct {
 	Name      string
 	Variables []Variable
 	Functions []Function
+	IsStruct  bool
 }
 
 func check(e error) {
@@ -45,40 +58,79 @@ func check(e error) {
 	}
 }
 
-// naive way to do this parse -- assumption is the file isn't too large so it reads in whole file at once...
-// TODO: chunked reading
+func parseClassBody(body *string, currentClass *Class) {
+	var rgx_ACCESS string = `^\s*(?:public|private|protected):`
+	var rgx_DECLFUNC string = `((?:\w+(?:::\w+)*(?:<(?:[^<>]|(?1))*>)?(?:\s*[*&])?\s+)+)(\w+)\s*\((.*?)\)(\s*const)?`
+	var rgx_DECLVAR string = `((?:\w+\s+)*)((?:[\w_:]|<(?:[^<>]|(?2))*>|,\s*)+)( *[*&] +| +[*&] *| +)*([A-Za-z_]\w*)`
+
+	re_ACCESS := regexp.MustCompile(rgx_ACCESS)
+	re_DECLFUNC := regexp.MustCompile(rgx_DECLFUNC)
+	re_DECLVAR := regexp.MustCompile(rgx_DECLVAR)
+
+	fmt.Printf("Body of %s\n-------\n%s\n", currentClass.Name, *body)
+
+	var currentAccess string = "private"
+	if currentClass.IsStruct {
+		currentAccess = "public"
+	}
+
+	scanner := bufio.NewScanner((strings.NewReader(*body)))
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if strings.TrimSpace(line) == "" {
+			continue
+		}
+
+		access := strings.TrimSpace(re_ACCESS.FindString(line))
+		if access != "" {
+			currentAccess = access
+			continue
+		}
+
+		function := re_DECLFUNC.FindString(line)
+		if function != "" {
+			// parse Function
+			var f Function
+			f.Visibility = currentAccess
+			continue
+		}
+
+		variable := re_DECLVAR.FindString(line)
+		if variable != "" {
+			// parse Variable
+			var v Variable
+			v.Visibility = currentAccess
+			continue
+		}
+
+	}
+
+	// match variables
+	for _, match := range re_DECLVAR.FindAllStringSubmatch(*body, -1) {
+		fmt.Printf("%s, %s\n", match[0], match[1])
+	}
+
+}
+
+// naive way to do this parse -- insane assumptions for now:
+// - the file isn't too large so it reads in whole file at once... TODO: chunked reading
+// - 1 expression per line...  TODO: regex-less parser
 func Parse(fileName string) {
 	fd, err := os.ReadFile(fileName)
 	check(err)
 
-	var rgxp_CLASS string = `(?s)(?:class|struct)\s+(\w+)\s*(:(?:\s*\w+\s+\w+\s*,?)*)?{([ -~\s]+?)};`
+	// Groups
+	// 1 - template;  	2 - template params; 3 - class/struct;
+	// 4 - class name; 	5 - inheritance;  	 6 - body
+	var rgxp_CLASS string = `(?s)(\s*template\s+<((?:typename|class)\s+\w+\s*,?\s*)+>)?\s*(class|struct)\s+(\w+)\s*(:(?:\s*\w+\s+\w+\s*,?)*)?{([ -~\s]+?)};`
 	re_CLASS := regexp.MustCompile(rgxp_CLASS)
-	for i, match := range re_CLASS.FindAllStringSubmatch(string(fd), -1) {
-		for j, cg := range match {
-			fmt.Printf("%d.%d-----------\n`%s`\n", i, j, cg)
+	for _, match := range re_CLASS.FindAllStringSubmatch(string(fd), -1) {
+		var currentClass Class
+		if match[3] == "struct" {
+			currentClass.IsStruct = true
 		}
+		currentClass.Name = match[4]
+		parseClassBody(&match[len(match)-1], &currentClass)
 	}
-
-	/*
-		fd, err := os.Open(fileName)
-		check(err)
-		defer fd.Close()
-
-		var i int = 0
-		scanner := bufio.NewScanner(fd)
-		for scanner.Scan() {
-			line := scanner.Text()
-			fmt.Printf("%d:\t%s\n", i, line)
-			i++
-		}
-
-		re, err := regexp.Compile(`\d+`)
-		if err != nil {
-			return
-		}
-
-		var x = re.FindString("skld234jf")
-		fmt.Println(x)
-	*/
-	//fmt.Println(currentLine)
 }

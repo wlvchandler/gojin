888ffb8d6e3936639aedb4cdc527b009fdc246f9
will <jaskamiin@gmail.com>
2024-07-17 23:32:44 -0400

    basic not-working ast

--- parafor/internal/ast/ast.go
+++ parafor/internal/ast/ast.go
@@ -1,10 +1,26 @@
 package ast
 
-import "localhost/wlvchandler/parafor/internal/token"
+import (
+	"localhost/wlvchandler/parafor/internal/token"
+)
 
 type Node struct {
 	Token    *token.Token
 	Children []*Node
+}
+
+type Node_Class struct {
+	m_name  string
+	m_vars  []*token.Token
+	m_funcs []*token.Token
+	m_patts []*token.Token
+}
+
+type Node_Field struct {
+	m_class *Node_Class
+	m_name  *token.Token
+	m_type  *token.Token
+	m_value *token.Token
 }
 
 func CreateRootNode() *Node {
@@ -16,13 +32,76 @@ func New(node_tok token.Token) *Node {
 	return n
 }
 
+// NODE CREATION
+
+func CreateNode(line []*token.Token) *Node {
+
+	switch line[0].Type {
+	case token.CLASS:
+
+		CreateNode_Class()
+	case token.FIELD:
+		CreateNode_Field()
+	case token.METHOD:
+		CreateNode_Method()
+
+	}
+
+	return &Node{}
+}
+
+// func CreateNode_Class(name string, vars []*token.Token, funcs []*token.Token, patterns []*token.Token) *Node_Class {
+func CreateNode_Class(line []*token.Token) *Node_Class {
+	var n Node_Class
+	for i, tok := range line {
+		switch tok.Type {
+		case token.NAME:
+			n.m_name = line[i+1].Literal
+		}
+	}
+	n.m_class = nil // TODO
+	return n
+	return &Node_Class{
+		m_name:  name,
+		m_vars:  vars,
+		m_funcs: funcs,
+		m_patts: patterns,
+	}
+}
+
+func CreateNode_Field(line []*token.Token) Node_Field {
+	var n Node_Field
+	for i, tok := range line {
+		switch tok.Type {
+		case token.NAME:
+			n.m_name = line[i+1]
+		case token.TYPE:
+			n.m_type = line[i+1]
+		case token.VALUE:
+			n.m_value = line[i+1]
+		}
+	}
+	n.m_class = nil // TODO
+	return n
+}
+
+func CreateNode_Pattern() *Node {
+	return &Node{}
+}
+
+func CreateNode_String() *Node {
+	return &Node{}
+}
+
+func CreateNode_Int() *Node {
+	return &Node{}
+}
+
 func IsLeafNode(node Node) bool {
 	return true
 }
 
-func CreateNode_Class() *Node {
-	return &Node{}
-}
+// FIND NODES IN TREE
 
 func findNode(name string, nodeType token.TokenType, root *Node) *Node {
 	var foundNode *Node
--- parafor/internal/ast/ast_test.go
+++ parafor/internal/ast/ast_test.go
@@ -12,20 +12,55 @@ func TestASTCreateRootNode(t *testing.T) {
 	}
 }
 
-func TestASTFindClassNode(t *testing.T) {
-	root := CreateRootNode()
+func TestASTCreateField(t *testing.T) {
+	n1 := token.Token{Type: token.FIELD, Literal: "field"}
+	n2 := token.Token{Type: token.CLASS, Literal: "class"}
+	n3 := token.Token{Type: token.IDENTIFIER, Literal: "User"}
+	n4 := token.Token{Type: token.NAME, Literal: "name"}
+	n5 := token.Token{Type: token.IDENTIFIER, Literal: "age"}
+	n6 := token.Token{Type: token.TYPE, Literal: "type"}
+	n7 := token.Token{Type: token.IDENTIFIER, Literal: "int"}
+	n8 := token.Token{Type: token.VALUE, Literal: "value"}
+	n9 := token.Token{Type: token.INT, Literal: "30"}
 
-	// field class:User name:age type:int value:30
-	n1 := New(token.Token{Type: token.FIELD, Literal: "field"})
-	n2 := New(token.Token{Type: token.CLASS, Literal: "class"})
-	n3 := New(token.Token{Type: token.IDENTIFIER, Literal: "User"})
-	n4 := New(token.Token{Type: token.NAME, Literal: "name"})
-	n5 := New(token.Token{Type: token.IDENTIFIER, Literal: "age"})
-	n6 := New(token.Token{Type: token.TYPE, Literal: "type"})
-	n7 := New(token.Token{Type: token.IDENTIFIER, Literal: "int"})
-	n8 := New(token.Token{Type: token.VALUE, Literal: "value"})
-	n9 := New(token.Token{Type: token.INT, Literal: "30"})
-
-	if root.Token != nil {
+	var line = []*token.Token{
+		&n1, &n2, &n3, &n4, &n5, &n6, &n7, &n8, &n9,
 	}
+
+	f := CreateNode_Field(line)
+
+	if f.m_class != nil {
+		t.Fatalf("Field class must be nil\n")
+	}
+	if f.m_name.Literal != n5.Literal {
+		t.Fatalf("Field name incorrect. Expected=%s\tFound=%s\n", n5.Literal, f.m_name.Literal)
+	}
+
+	if f.m_type.Literal != n7.Literal {
+		t.Fatalf("Field type incorrect. Expected=%s\tFound=%s\n", n7.Literal, f.m_type.Literal)
+	}
+	if f.m_value.Literal != n9.Literal {
+		t.Fatalf("Field value incorrect. Expected=%s\tFound=%s\n", n9.Literal, f.m_value.Literal)
+	}
+
+}
+
+func TestASTFindClassNode(t *testing.T) {
+	/*
+		root := CreateRootNode()
+
+		// field class:User name:age type:int value:30
+		n1 := New(token.Token{Type: token.FIELD, Literal: "field"})
+		n2 := New(token.Token{Type: token.CLASS, Literal: "class"})
+		n3 := New(token.Token{Type: token.IDENTIFIER, Literal: "User"})
+		n4 := New(token.Token{Type: token.NAME, Literal: "name"})
+		n5 := New(token.Token{Type: token.IDENTIFIER, Literal: "age"})
+		n6 := New(token.Token{Type: token.TYPE, Literal: "type"})
+		n7 := New(token.Token{Type: token.IDENTIFIER, Literal: "int"})
+		n8 := New(token.Token{Type: token.VALUE, Literal: "value"})
+		n9 := New(token.Token{Type: token.INT, Literal: "30"})
+
+		if root.Token != nil {
+		}
+	*/
 }
